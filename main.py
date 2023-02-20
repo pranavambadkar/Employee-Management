d979fa03545fdedc38e5ad7d68603ff8b1ba0e28
Pranav Ambadkar <125912673+pranavambadkar@users.noreply.github.com>
2023-02-21 00:05:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #Project -- Employee Management System using Python
+#It inlcudes all the validation while creating and updating the employee data
 #Developer -- Pranav Ambadkar
 
 from tkinter import *
@@ -353,4 +354,4 @@ dw.withdraw()
 
 root.mainloop()
 
-#Develop by Pranav
+#Developed by Pranav
